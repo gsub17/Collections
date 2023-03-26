983f29407d6b11fbf6f2420ef7f13ad62b6b9c0d
Subhajit Ghosh <s.ghoshsovutz@gmail.com>
2023-03-26 12:07:01 +0530

    Update Helloworld.java

--- src/main/java/Testing_git_commands/Helloworld.java
+++ src/main/java/Testing_git_commands/Helloworld.java
@@ -1,14 +1,5 @@
 package Testing_git_commands;
 
-public class Helloworld {
-
-    // change by branch usr/subhajit2 1
-    // 1st change
-    // 2nd change
-  // change made by remote.
-  // remote1
-  // remote2
-    
-    //new changes by remote
-
+public class Helloworld{
+  // making changes via remote
 }
